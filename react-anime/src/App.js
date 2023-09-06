61f0c4499b739ba14e72c0ef4e62fc210963820a
0112345 <ntwarifiacre043@gmail.com>
2023-09-06 18:32:35 +0200

    initial commit for react js

---
+++ react-anime/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ntwari Fiacre</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-anime/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+
+function NavBar() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+            
+        </div>
+
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
